4d9d9719d5c0803684d1e54ac1f180cee85fef31
Silenc56 <110986249+Silenc56@users.noreply.github.com>
2022-08-20 15:14:16 +0300

    GoTrip

---
+++ js/mani.js
@@ -0,0 +1,37 @@
+const owl = $(".owl-carousel");
+owl.owlCarousel({
+  center: true,
+  loop: true,
+  margin: 30,
+  startPosition: 1,
+  items: 3,
+  responsive: {
+    850: {
+      items: 3,
+    },
+    1000: {
+      items: 3,
+      margin: 20,
+    },
+    1200: {
+      margin: 30,
+    },
+  },
+});
+$(".slider__btn--prev").click(function () {
+  owl.trigger("prev.owl.carousel");
+});
+
+$(".slider__btn--next").click(function () {
+  owl.trigger("next.owl.carousel");
+});
+
+const navBtn = document.querySelector(".nav__toggle");
+const nav = document.querySelector(".nav");
+const menuIcon = document.querySelector(".menu-icon");
+
+navBtn.onclick = function () {
+  nav.classList.toggle("nav--mobile");
+  menuIcon.classList.toggle("menu-icon-active");
+  document.body.classList.toggle("no-scrole");
+};
